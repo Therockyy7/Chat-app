02a924c967cb4f1661674f11d1d62e17fea7b621
tran nguyen hoang phuc <tphuc9398@gmail.com>
2024-12-25 16:51:50 +0700

    Deloyment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
@@ -10,6 +11,8 @@ import { connectToMongoDB } from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -24,6 +27,11 @@ app.get("/", (req, res) => {
     res.send("Hello");
 })
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
